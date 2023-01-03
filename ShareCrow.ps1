fbdfc1208a3568745d5de244a061954d1cb33a04
CBHue <CBHue@users.noreply.github.com>
2023-01-03 09:54:25 -0500

    Update ShareCrow.ps1

--- ShareCrow.ps1
+++ ShareCrow.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
   .SYNOPSIS
 
     ShareCrow.ps1
@@ -36,20 +36,24 @@
 
     The directory to output the results. 
 
-  .PARAMETER Threads
+  .PARAMETER maxWorkers
 
-    (Placeholder for multi-threading to be added in later)
+    Maximum threads to run
+
+  .PARAMETER testFile
+
+    Name of the test file to create during write check. Default: sclog_<date>.txt
 
   .EXAMPLE
 
-    C:\PS>  .\ShareCrow.ps1 -accessCheck -inputFile .\targets\smbHosts.txt -outputDir .\SC 
+    C:\PS>  .\ShareCrow.ps1 -accessCheck -inputFile .\targets\smbHosts.txt -outputDir 'C:\Users\Me\Temp\'
 
     Description
     -----------
     This command will perform a net view on the target host, then run test-path on each item to find readable paths. For each readable path it will list the contents. 
 
   .EXAMPLE
-    C:\PS>  .\ShareCrow.ps1 -writeCheck -inputFile .\targets\accessibleShares.txt -outputDir .\SC 
+    C:\PS>  .\ShareCrow.ps1 -writeCheck -inputFile .\targets\accessibleShares.txt -outputDir 'C:\Users\Me\Temp\' 
     
     Description
     -----------
@@ -61,6 +65,9 @@ Param
   (
     # Check if you can write
     [switch]$writeCheck  = $false,
+    
+    # filename to test write
+    [string]$testFile = "sclog_$(get-date -f MM-dd-yy_HHmmss).txt",
 
     # Check if you can access 
     [switch]$accessCheck = $false,
@@ -69,12 +76,20 @@ Param
     [string]$inputFile,
     
     [Parameter(Mandatory = $true)]
-    [string]$outputDir
+    [string]$outputDir,
+
+    # Worker Dem
+    [parameter(Mandatory=$false)]
+    [ValidateRange(3,10000)]
+    [System.Int32]
+    # Max number of threads for RunspacePool.
+    $maxWorkers = 10
     
   )
 
-$CBH = @"
-ShareCrow v1.0 - CBHue
+$banner = @"
+
+ShareCrow v2.0 - CBHue
                               __
                               | \      _
                            ==='=='==  (o>
@@ -90,194 +105,138 @@ ShareCrow v1.0 - CBHue
 
 "@
 
-Function getShares {
-    Param($smb)
-    $SMBArray = New-Object System.Collections.ArrayList
-
-    # Turn off errors for this bc i dont care
-    $ErrorActionPreference = "SilentlyContinue"; 
- 
-    net view \\$smb /all | %{
-        if($_.IndexOf(' Disk ') -gt 0) {
-            $shareName = $_.Split('      ')[0]
-            $FP = Join-Path $smb $shareName;
-            $FP = "\\" + $FP
-
-            if (Test-Path $FP -ErrorAction SilentlyContinue) {
-                $null = $SMBArray.Add($FP)
-                $null = $AllShares.Add($FP)
-            }
-        }
-    }
-
-    # might care about other errors lol
-    $ErrorActionPreference = "Continue";
-
-
-    return $SMBArray 
-}
-
-Function listContents { 
-    Param($share)
-    $Folders = Get-ChildItem $share -Force | Sort-Object LastWriteTime
-    $null = $AllDetail.Add($Folders)
-    return $Folders
-}
-
-Function test-create {
-    Param($DIR)
-    $file = $DIR + "\_c8h.txt"
-    try   { Out-File -FilePath $file }
-    catch {   }
-    
-    # check if file exists
-    if(Test-Path -ErrorAction SilentlyContinue $file){
-        Write-Host -NoNewline "[!] Writable Directory    : " 
-        Write-Host $file -ForegroundColor Green
-        $global:counter++
-        $null = $AllShares.Add($DIR)
-
-        # delete the file
-        Write-Host "[*] Deleting created file : $file"
-        try 
-        {
-            Remove-Item $file
-        }
-        catch 
-        {
-            Write-Host -NoNewline "[!] Error deleteing file  : "
-            Write-Host $file -BackgroundColor Black -ForegroundColor red
-        }
-    }
-}
-
-#######################
-#                     #
-# Do the work here!!! #
-#                     #
-#######################
+# Store the results
+$CBH = [hashtable]::Synchronized([ordered]@{})
 
 # check what were supposed to do. 
 if ((!$writeCheck -and !$accessCheck) -or ($writeCheck -and $accessCheck)){ 
     write-Host "[!] You must choose -accessCheck OR -writeCheck" -ForegroundColor Red -BackgroundColor Black
-    write-Host "`t.\ShareCrow.ps1 -accessCheck -inputFile .\targets\smb3.txt -outputDir .\SC"
-    write-Host "`t.\ShareCrow.ps1 -writeCheck -inputFile .\targets\accessibleShares.txt -outputDir .\SC"
+    write-Host "`t.\ShareCrow.ps1 -accessCheck -inputFile .\targets\smb3.txt -outputDir .\shareCrow"
+    write-Host "`t.\ShareCrow.ps1 -writeCheck -inputFile .\targets\accessibleShares.txt -outputDir .\shareCrow"
     exit 
 }
 
 $module = "unknown"
-if ($accessCheck) {$module = "_accessCheck_"}
-else              {$module = "_writeCheck_"}
+if ($accessCheck) {
+    $CBH.add("accessCheck","accessCheck")
+    $module = $CBH["accessCheck"]
+    }
+else              {
+    $CBH.add("writeCheck",$testFile)
+    $module = "writeCheck"
+    }
 
-write-Host $CBH
-write-Host "Welcome to ShareCrow      : " -NoNewline 
-write-Host $module.Trim("_","*") -ForegroundColor Green
-
-# Globals and thangs
-$global:AllShares = New-Object System.Collections.ArrayList
-$global:AllDetail = New-Object System.Collections.ArrayList
-$global:counter = 0
+write-Host $banner
+write-Host ("[!] Welcome to ShareCrow").PadRight(40," ") " : " -NoNewline 
+write-Host $module -ForegroundColor Green
+write-Host ("[!] Pooled Workers").PadRight(40," ") " : " -NoNewline 
+write-Host $maxWorkers -ForegroundColor Green
 
 # Output file
-$outBasics = $outputDir + "\ShareCrow" + $module + "$(get-date -f MM-dd-yyyy_HH_mm_ss)" + ".txt"
-$outDetail = $outputDir + "\ShareCrow" + $module + "$(get-date -f MM-dd-yyyy_HH_mm_ss)" + "_Details.txt"
+$outBasics = $outputDir + "\ShareCrow_" + $module + "_$(get-date -f MM-dd-yyyy_HH_mm_ss)" + ".csv"
+$outDetail = $outputDir + "\ShareCrow_" + $module + "_$(get-date -f MM-dd-yyyy_HH_mm_ss)" + "_Details.txt"
 
-# Input File
-$targets = get-content $inputFile
+# Define the workers job
+$Worker = {
+    Param($target, $CBH)
+    #Start-Sleep -Seconds 5 # Doing some work....
 
-# This is kinda hacky ... getting percentage of each loop: 1/Total * 100 = Each Loops %
-$Count = $Total = $tPerc = 0
-$Total = $targets.Count
-$oPerc = (1 / $Total) * 100
-
-# If you are doing an access check 
-if ($accessCheck) {
-    
-    # Write module to outfile
-    $CBH | Out-File $outDetail
-    "Module: " + $module.Trim('_','*') | Out-File $outDetail -Append
-    " "  | Out-File $outDetail -Append
-
-    write-Host "We have work to do        : " -NoNewline
-    write-Host $Total -NoNewline -ForegroundColor green
-    Write-Host " Hosts" -ForegroundColor green
-     
-    foreach ($smb in $targets) {
-        # Give a status and a percentage complete
-        $Total = $Total - 1
-        $tPerc = $tPerc + $oPerc
-        $dperc = [Math]::Round($tPerc)
-        Write-Progress -Activity "Testing Ports: $hst ... Hosts Left: $Total ..." -Status "Percentage complete: $dperc % " -PercentComplete $dperc   
-
-        Write-Host ""
-        Write-Host -NoNewline "[*] Checking access to    : "
-        Write-Host $smb -ForegroundColor Yellow
-        $SMBA = New-Object System.Collections.ArrayList
-        $SMBA = getShares $smb
-
-        if ($SMBA.Count -lt 1){
-            Write-Host -NoNewline "[!] No shares accessible  : "
-            Write-Host $smb -BackgroundColor black -ForegroundColor red
+    Try {
+        # Lets check our access ... 
+        if ($CBH.ContainsKey("accessCheck")) {
+            
+            $shares = net view \\$target /all | select -Skip 7 | ?{$_ -match 'disk*'} | %{$_ -match '^(.+?)\s+Disk*'|out-null;$matches[1]}
+            $shares | % { 
+                $FP = "\\" + $target + "\" + $_
+                Write-Host ("[*] Processing").PadRight(40," ") " : " -NoNewline
+                Write-Host -ForegroundColor Green  $FP
+                if (Test-Path $FP) {
+                    $folders = Get-ChildItem $FP -Force -Verbose | Sort-Object LastWriteTime
+                    $CBH.add($FP,$folders)
+                }
+            } # End Share Loop
         }
-
-        foreach ($share in $SMBA) {
-            if (!$share){ continue }
-            Write-Host -NoNewline "[*] Listing contents      : "
-            Write-Host $share -ForegroundColor green
-            listContents $share
-        }
+        # Only other option is writeCheck ...
+        else {
+            Write-Host ("[*] Processing").PadRight(40," ") " : " -NoNewline
+            Write-Host -ForegroundColor Green  $target      
+            $dirs = Get-ChildItem $target -Directory -Recurse
+            foreach ($d in $dirs) {
+                $file = $d.FullName + "\" + $CBH["writeCheck"]
+                try   { 
+                    Out-File -FilePath $file 
+                }
+                catch {   
+                
+                }
+                if(Test-Path -ErrorAction SilentlyContinue $file){
+                    $CBH.add($file,1)
+                
+                    # Ok it worked .... now delete the file
+                    #Write-Host "[*] Deleting created file : $file"
+                    try   { 
+                        Remove-Item $file 
+                        $CBH[$file] = "$file"
+                    }
+                    catch {
+                        $CBH[$file] = "Error Deleting $file" 
+                    }
+                }
+            }
+        } 
     }
-
-    $global:AllShares | Out-File $outBasics
-    Write-Host -NoNewline "`n[*] All accessible shares written to        : "
-    Write-Host "$outBasics" -ForegroundColor green
-
-    $global:AllDetail | Out-File $outDetail -Append
-    Write-Host -NoNewline "[*] All accessible share details written to : "  
-    Write-Host "$outDetail" -ForegroundColor green
+    Catch {
+        $CBH.add($target, $_.Exception.Message)
+    }
 }
 
-if ($writeCheck) {
+# Create the pool
+$SessionState = [System.Management.Automation.Runspaces.InitialSessionState]::CreateDefault()
+$RunspacePool = [RunspaceFactory]::CreateRunspacePool(1, $maxWorkers, $SessionState, $Host)
+$RunspacePool.Open()
+$Jobs = New-Object System.Collections.ArrayList
 
-    # Write module to outfile
-    $CBH | Out-File $outBasics
-    "Module: " + $module.Trim('_','*') | Out-File $outBasics -Append
-    " "  | Out-File $outBasics -Append
+# Read list of Hosts to check for open shares
+$targets = Get-Content $inputFile
 
-    write-Host "We have work to do        : " -NoNewline
-    write-Host $Total -NoNewline -ForegroundColor green
-    Write-Host " Shares" -ForegroundColor green
-
-    # loop thru each provided share
-    foreach ($share in $targets) {
-        
-        # Give a status and a percentage complete
-        $Total = $Total - 1
-        $tPerc = $tPerc + $oPerc
-        $dperc = [Math]::Round($tPerc)
-        Write-Progress -Activity "Getting Shares: $hst ... Hosts Left: $Total ..." -Status "Percentage complete: $dperc % " -PercentComplete $dperc  
-        
-        # get all directories in the share ... this may take a while depending on how deep it is
-        Write-Host -NoNewline "`n[*] Attempting to write   : " 
-        Write-Host $share"\*" -ForegroundColor Yellow
-        $dirs = Get-ChildItem $share -Directory -Recurse
-
-        $global:counter = 0
-        # try to create a file in each directory
-        foreach ($d in $dirs) {
-            test-create $d.FullName
-        }
-        Write-Host -NoNewline "[*] Writeable Directories : "
-        Write-Host $global:counter -ForegroundColor Yellow
-        $global:counter = 0
-    }
+# Task workers to do all the things
+foreach ($target in $targets) {
+    Write-Host ("[*] Creating runspace for").PadRight(40," ") " : " -NoNewline
+    Write-Host -ForegroundColor Green $target
+    $PowerShell = [powershell]::Create()
+	$PowerShell.RunspacePool = $RunspacePool
+    $PowerShell.AddScript($Worker).AddArgument($target).AddArgument($CBH) | Out-Null
     
-    if ($global:AllShares.Count -lt 1){
-        Write-Host "`n[*] No writable shares    : " -ForegroundColor Red
+    $JobObj = New-Object -TypeName PSObject -Property @{
+		Runspace = $PowerShell.BeginInvoke()
+		PowerShell = $PowerShell  
     }
-    else {
-        $global:AllShares | Out-File -Append $outBasics
-        Write-Host -NoNewline "`n[*] All writeable shares saved to : "
-        Write-Host "$outBasics" -ForegroundColor green
-    }
+
+    $Jobs.Add($JobObj) | Out-Null
 }
+
+# Check on the status
+while ($Jobs.Runspace.IsCompleted -contains $false) {
+    #Write-Host (Get-date).Tostring() "Still running..."
+    Write-Verbose -Message "Jobs in progress $($Jobs.Count)"
+	Start-Sleep 1
+}
+
+# Print the results
+# Write module to outfile
+$banner | Out-File $outDetail
+"Module: " + $CBH[0] | Out-File $outDetail -Append
+" "  | Out-File $outDetail -Append
+
+# write the basics
+$CBH.GetEnumerator()  | Select Key | Export-CSV -path $outBasics -NoTypeInformation
+
+# write the details
+$CBH.GetEnumerator() | ForEach-Object {
+     $_.value | Out-File $outDetail -Append
+}
+
+Write-Host ("[*] All basic results written to ").PadRight(40," ") " : " -NoNewline
+Write-Host "$outBasics" -ForegroundColor green
+Write-Host ("[*] All detailed results written to ").PadRight(40," ") " : "  -NoNewline
+Write-Host "$outDetail" -ForegroundColor green
